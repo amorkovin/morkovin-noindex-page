bb556c89a4642ad21fce611caa586e6bc5317dd9
Andey Morkovin <amorkovin@gmail.com>
2020-05-05 20:01:16 +0300

    Удалил теги, разрешающие индексацию от Yoast SEO
    Плагин Yoast SEO версии 14.0.4 начал выводить в head теги, разрещающие индексацию в том числе и пагинационных страниц. В итоге внутренние пагинационные страницы стали садиться в индекс.
    
    Для внутренних пагинационных страниц я удалил эти теги из head.

--- morkovin-noindex-page.php
+++ morkovin-noindex-page.php
@@ -5,11 +5,11 @@
   Author: Andrey Morkovin
   Plugin URI: https://t.me/morkovin_verstka
   Author URI: http://www.sdelaysite.com
-  Version: 1.4
+  Version: 1.5
  */
 
-add_action( 'wp_head', 'head_seo_meta_tags' );
-function head_seo_meta_tags(){
+add_action( 'wp_head', 'mnp_head_seo_meta_tags' );
+function mnp_head_seo_meta_tags(){
 	if ( is_archive() or is_front_page() ) {
 		$paged = (get_query_var('paged')) ? get_query_var('paged') : 1;
 		if ($paged > 1) {
@@ -18,4 +18,24 @@ function head_seo_meta_tags(){
 			echo "\n<!--/Морковный ноиндекс-->\n\n";
 		}
 	}
+}
+
+add_action('wp_head','mnp_remove_yoast_meta_index_ob_start',0);
+function mnp_remove_yoast_meta_index_ob_start() {
+    ob_start();
+}
+add_action('wp_head','mnp_remove_yoast_meta_index_get_clean', PHP_INT_MAX);
+function mnp_remove_yoast_meta_index_get_clean() {
+    $wp_head_content = ob_get_clean();
+
+    if ( is_archive() or is_front_page() ) {
+		$paged = (get_query_var('paged')) ? get_query_var('paged') : 1;
+		if ($paged > 1) {
+			$wp_head_content = str_replace('<meta name="robots" content="index, follow" />', '', $wp_head_content);
+			$wp_head_content = str_replace('<meta name="googlebot" content="index, follow, max-snippet:-1, max-image-preview:large, max-video-preview:-1" />', '', $wp_head_content);
+			$wp_head_content = str_replace('<meta name="bingbot" content="index, follow, max-snippet:-1, max-image-preview:large, max-video-preview:-1" />', '', $wp_head_content);
+		}
+	}
+       
+    echo $wp_head_content;
 }
